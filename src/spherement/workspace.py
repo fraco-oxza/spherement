9cb54ed9e6a086b2d8f7b4b1d09e419d22d3aefd
Francisco Carvajal <fraco_oxza@outlook.com>
2024-02-11 23:22:50 -0300

    code: improves in code quality

--- src/spherement/workspace.py
+++ src/spherement/workspace.py
@@ -28,9 +28,13 @@ class DrawPoint:
         )
 
     @staticmethod
-    def from_indexes(indexes: tuple[float, float]) -> DrawPoint:
+    def from_indexes(indexes: tuple[float, float] | list[float]) -> DrawPoint:
         x, y = indexes
         return DrawPoint(math.atan2(-y, x), math.sqrt(x**2 + y**2))
+
+    @staticmethod
+    def from_point_2d(point: Point2d) -> DrawPoint:
+        return DrawPoint.from_indexes((point.x, point.y))
 
     def __mul__(self, other: float) -> DrawPoint:
         return DrawPoint(self.angle, self.distance * other)
@@ -52,7 +56,45 @@ class DrawPoint:
         return self + other
 
 
-class Point:
+class Point2d:
+    x: float
+    y: float
+
+    def __init__(self, x: float, y: float):
+        self.x = x
+        self.y = y
+
+    def distance(self, p: Point2d):
+        return math.sqrt((self.x - p.x) ** 2 + (self.y - p.y) ** 2)
+
+    def module(self):
+        return math.sqrt(self.x**2 + self.y**2)
+
+    def normalize(self) -> Point2d:
+        module = self.module()
+        return Point2d(self.x / module, self.y / module)
+
+    def get_indexes(self) -> tuple[float, float]:
+        return (self.x, self.y)
+
+    @staticmethod
+    def add_points(points: list[Point2d]) -> Point2d:
+        x = sum([p.x for p in points])
+        y = sum([p.y for p in points])
+        return Point2d(x, y)
+
+    @staticmethod
+    def from_draw_point(draw_point: DrawPoint) -> Point2d:
+        return Point2d(*draw_point.get_indexes())
+
+    def __mul__(self, other: float) -> Point2d:
+        return Point2d(self.x * other, self.y * other)
+
+    def __rmul__(self, other: float) -> Point2d:
+        return self * other
+
+
+class Point3d:
     x: float
     y: float
     z: float
@@ -62,7 +104,7 @@ class Point:
         self.y = y
         self.z = z
 
-    def distance(self, p: Point):
+    def distance(self, p: Point3d):
         return math.sqrt(
             (self.x - p.x) ** 2 + (self.y - p.y) ** 2 + (self.z - p.z) ** 2
         )
@@ -70,16 +112,16 @@ class Point:
     def module(self):
         return math.sqrt(self.x**2 + self.y**2 + self.z**2)
 
-    def normalize(self) -> Point:
+    def normalize(self) -> Point3d:
         module = self.module()
-        return Point(self.x / module, self.y / module, self.z / module)
+        return Point3d(self.x / module, self.y / module, self.z / module)
 
 
 class Line:
-    p1: Point
-    p2: Point
+    p1: Point3d
+    p2: Point3d
 
-    def __init__(self, p1: Point, p2: Point):
+    def __init__(self, p1: Point3d, p2: Point3d):
         self.p1 = p1
         self.p2 = p2
 
@@ -99,10 +141,10 @@ class Workspace:
     width: int
     image: Optional[pygame.Surface]
     image_pos: DrawPoint
-    image_offset: list[float]
+    image_offset: Point2d
     scale: float
     radius: float
-    view_point: list[float]
+    view_point: Point2d
     points: list[DrawPoint]
     stage: Stage
 
@@ -113,27 +155,23 @@ class Workspace:
         self.width = width
         self.image = None
         self.scale = 1
-        self.view_point = [0, 0]
-        self.image_offset = [0, 0]
+        self.view_point = Point2d(0, 0)
+        self.image_offset = Point2d(0, 0)
         self.radius = min(self.width, self.height) / 2
         self.points = []
         self.stage = Stage.ADJUSTMENT
+        self.center = Point2d(self.width / 2, self.height / 2)
 
     def set_image(self, image: pygame.Surface):
         self.image = image
-        w, h = self.image.get_size()
-        self.view_point = [0, 0]
+        self.view_point = Point2d(0, 0)
         self.scale = 1.0
 
-    def to_local_coords(self, coord: list[int] | tuple[int, int]):
-        return (coord[0] - self.left, coord[1] - self.top)
-
-    @staticmethod
-    def add_points(p1, p2):
-        return [p1[0] + p2[0], p1[1] + p2[1]]
+    def to_local_coords(self, coord: list[int] | tuple[int, int]) -> Point2d:
+        return Point2d(coord[0] - self.left, coord[1] - self.top)
 
     def pov(self):
-        return self.add_points(self.view_point, (self.width / 2, self.height / 2))
+        return Point2d.add_points([self.view_point, self.center])
 
     def draw(self, screen: pygame.Surface):
         surf = pygame.Surface((self.width, self.height))
@@ -142,9 +180,9 @@ class Workspace:
         if self.image:
             surf.blit(
                 pygame.transform.scale_by(self.image, self.scale),
-                self.add_points(
-                    [*map(lambda x: self.scale * x, self.image_offset)], self.pov()
-                ),
+                Point2d.add_points(
+                    [self.scale * self.image_offset, self.pov()]
+                ).get_indexes(),
             )
 
         circle_radius = self.radius * (
@@ -153,7 +191,7 @@ class Workspace:
         pygame.draw.circle(
             surf,
             "#00FF00",
-            self.pov(),
+            self.pov().get_indexes(),
             circle_radius,
             1,
         )
@@ -162,7 +200,9 @@ class Workspace:
             pygame.draw.circle(
                 surf,
                 "#FF0000",
-                self.add_points(self.pov(), (self.scale * draw_point).get_indexes()),
+                Point2d.add_points(
+                    [self.pov(), Point2d.from_draw_point(self.scale * draw_point)]
+                ).get_indexes(),
                 5,
             )
 
@@ -174,18 +214,22 @@ class Workspace:
             pygame.draw.line(
                 surf,
                 "#FF0000",
-                self.add_points(self.pov(), (self.scale * p1).get_indexes()),
-                self.add_points(self.pov(), (self.scale * p2).get_indexes()),
+                Point2d.add_points(
+                    [self.pov(), Point2d.from_draw_point(self.scale * p1)]
+                ).get_indexes(),
+                Point2d.add_points(
+                    [self.pov(), Point2d.from_draw_point(self.scale * p2)]
+                ).get_indexes(),
                 1,
             )
 
             p1_x, p1_y = p1.get_indexes()
             p1_x, p1_y = p1_x / self.radius, p1_y / self.radius
-            p1_3d = Point(p1_x, p1_y, math.sqrt(1 - p1_x**2 - p1_y**2))
+            p1_3d = Point3d(p1_x, p1_y, math.sqrt(1 - p1_x**2 - p1_y**2))
 
             p2_x, p2_y = p2.get_indexes()
             p2_x, p2_y = p2_x / self.radius, p2_y / self.radius
-            p2_3d = Point(p2_x, p2_y, math.sqrt(1 - p2_x**2 - p2_y**2))
+            p2_3d = Point3d(p2_x, p2_y, math.sqrt(1 - p2_x**2 - p2_y**2))
 
             print(
                 "LINE Nº ",
@@ -207,10 +251,10 @@ class Workspace:
             # write in the center of the line
             surf.blit(
                 text,
-                self.add_points(
-                    self.add_points(
+                Point2d.add_points(
+                    [
                         self.pov(),
-                        (
+                        Point2d.from_draw_point(
                             DrawPoint.from_indexes(
                                 (
                                     (p1.get_indexes()[0] + p2.get_indexes()[0]) / 2,
@@ -218,10 +262,10 @@ class Workspace:
                                 )
                             )
                             * self.scale
-                        ).get_indexes(),
-                    ),
-                    (-10, -10),
-                ),
+                        ),
+                        Point2d(-10, -10),
+                    ]
+                ).get_indexes(),
             )
 
         screen.blit(surf, (self.left, self.top))
@@ -230,9 +274,8 @@ class Workspace:
         if not self.image:
             return
 
-        p = DrawPoint.from_indexes(self.view_point)
-        self.view_point = [*(p * delta).get_indexes()]
-
+        p = DrawPoint.from_point_2d(self.view_point)
+        self.view_point = Point2d.from_draw_point(p * delta)
         self.scale *= delta
 
     def move_view(self):
@@ -243,13 +286,13 @@ class Workspace:
             multiplier = 5.0
 
         if keys[pygame.K_UP]:
-            self.view_point[1] += 1 * self.scale * multiplier
+            self.view_point.y += 1 * self.scale * multiplier
         if keys[pygame.K_DOWN]:
-            self.view_point[1] -= 1 * self.scale * multiplier
+            self.view_point.y -= 1 * self.scale * multiplier
         if keys[pygame.K_LEFT]:
-            self.view_point[0] += 1 * self.scale * multiplier
+            self.view_point.x += 1 * self.scale * multiplier
         if keys[pygame.K_RIGHT]:
-            self.view_point[0] -= 1 * self.scale * multiplier
+            self.view_point.x -= 1 * self.scale * multiplier
         if keys[pygame.K_MINUS]:
             self.change_scale(0.99**multiplier)
         if keys[pygame.K_PLUS]:
@@ -264,13 +307,13 @@ class Workspace:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     self.points.append(
-                        DrawPoint.from_indexes(
-                            self.add_points(
-                                self.to_local_coords(event.pos),
-                                self.add_points(
-                                    (-self.view_point[0], -self.view_point[1]),
-                                    (-self.width / 2, -self.height / 2),
-                                ),
+                        DrawPoint.from_point_2d(
+                            Point2d.add_points(
+                                [
+                                    self.to_local_coords(event.pos),
+                                    -1 * self.view_point,
+                                    -1 * self.center,
+                                ],
                             ),
                         )
                         * (1 / self.scale)
@@ -280,26 +323,19 @@ class Workspace:
                         self.points.pop()
                         print("Point out of range")
 
-                    # x, y = self.points[-1].get_indexes()
-                # x = x / self.radius
-                # y = y / self.radius
-                # if x**2 + y**2 > 1:
-                # continue
-                # point3d = Point(x, y, math.sqrt(1 - x**2 - y**2))
-                # print(math.degrees(Line(Point(0, 0, 1), point3d).angle()))
                 elif event.button == 3:
                     self.points.pop()
 
     def adjust_view(self, events: list[pygame.event.Event]):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP]:
-            self.image_offset[1] += 1
+            self.image_offset.y += 1
         if keys[pygame.K_DOWN]:
-            self.image_offset[1] -= 1
+            self.image_offset.y -= 1
         if keys[pygame.K_LEFT]:
-            self.image_offset[0] += 1
+            self.image_offset.x += 1
         if keys[pygame.K_RIGHT]:
-            self.image_offset[0] -= 1
+            self.image_offset.x -= 1
         if keys[pygame.K_MINUS]:
             self.change_scale(0.99)
         if keys[pygame.K_PLUS]:
@@ -308,16 +344,19 @@ class Workspace:
         for event in events:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN:
-                    self.stage = Stage.MEASUREMENT
-                    self.image = pygame.transform.scale_by(self.image, self.scale)
-                    h, w = self.image.get_size()
-                    self.image_offset[0] += (h / self.scale - h) / 2
-                    self.image_offset[1] += (w / self.scale - w) / 2
-                    self.scale = 1.0
+                    if self.image is not None:
+                        self.stage = Stage.MEASUREMENT
+                        self.image = pygame.transform.scale_by(self.image, self.scale)
+                        h, w = self.image.get_size()
+                        self.image_offset.x += (h / self.scale - h) / 2
+                        self.image_offset.y += (w / self.scale - w) / 2
+                        self.scale = 1.0
 
     def update(self, events: list[pygame.event.Event]):
         if not self.image:
-            self.set_image(pygame.image.load(ask_file()))
+            file = ask_file()
+            if file is not None:
+                self.set_image(pygame.image.load(file))
         if self.stage == Stage.ADJUSTMENT and True:
             self.adjust_view(events)
         elif self.stage == Stage.MEASUREMENT:
